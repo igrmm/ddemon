746d42103a524ebf076aef1aefab3de6fba987c1
igrmm <igrmmdev@gmail.com>
2024-03-16 23:08:31 -0400

    add valgrind

--- run.sh
+++ run.sh
@@ -2,6 +2,23 @@
 
 mkdir -p build
 cd build
+
+if [ "$1" = "valgrind" ]; then
+    cd ../assets
+    valgrind \
+        --suppressions=../valgrind.sup \
+        --leak-check=full \
+        --show-reachable=yes \
+        --show-leak-kinds=all \
+        --error-limit=no \
+        --gen-suppressions=all \
+        --track-origins=yes \
+        --keep-debuginfo=yes \
+        --log-file=../build/supdata.log \
+        ../build/ddemon
+    exit
+fi
+
 if [ "$1" = "debug" ]; then
     export SANITIZERS="-fsanitize=address,undefined"
     cmake -DCMAKE_BUILD_TYPE=Debug ..
