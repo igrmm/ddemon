1f95a985e31c3a98aef37a5037221c476de29669
igrmm <igrmmdev@gmail.com>
2024-11-09 10:57:29 -0400

    rename ui_window variable with window

--- src/ui.c
+++ src/ui.c
@@ -9,37 +9,37 @@
 #define UI_MINIM_BUTTON_CODEPOINT 8854
 #define UI_MAXIM_BUTTON_CODEPOINT 8853
 
-void ui_mk_window(struct ui_window *ui_window, struct assets *assets,
+void ui_mk_window(struct ui_window *window, struct assets *assets,
                   struct core *core)
 {
     SDL_FRect pixel_tex_region;
     assets_get_texture_region(assets, ASSET_TEXTURE_PIXEL, &pixel_tex_region);
 
     // draw window
-    core_add_drawing_fill_rect(core, &pixel_tex_region, &ui_window->rect,
-                               &ui_window->bg_color);
+    core_add_drawing_fill_rect(core, &pixel_tex_region, &window->rect,
+                               &window->bg_color);
 
     // draw bar
     int bar_height = txt_get_font_height(assets->fonts[ASSET_FONT_SMALL]);
-    SDL_FRect bar_rect = {ui_window->rect.x,
-                          ui_window->rect.y + ui_window->rect.h - bar_height,
-                          ui_window->rect.w, bar_height};
+    SDL_FRect bar_rect = {window->rect.x,
+                          window->rect.y + window->rect.h - bar_height,
+                          window->rect.w, bar_height};
     core_add_drawing_fill_rect(core, &pixel_tex_region, &bar_rect,
-                               &ui_window->fg_color);
+                               &window->fg_color);
 
     // draw window border
-    core_add_drawing_rect(core, &pixel_tex_region, &ui_window->rect,
-                          &ui_window->fg_color, 1);
+    core_add_drawing_rect(core, &pixel_tex_region, &window->rect,
+                          &window->fg_color, 1);
 
     // draw scale button
     SDL_FRect scale_btn_rect = {
-        ui_window->rect.x + ui_window->rect.w - bar_height / 2.0f - 2,
-        ui_window->rect.y + 2, bar_height / 2.0f, bar_height / 2.0f};
+        window->rect.x + window->rect.w - bar_height / 2.0f - 2,
+        window->rect.y + 2, bar_height / 2.0f, bar_height / 2.0f};
     core_add_drawing_fill_rect(core, &pixel_tex_region, &scale_btn_rect,
-                               &ui_window->fg_color);
+                               &window->fg_color);
 
     // draw window title on bar
-    txt(ui_window->title, bar_rect.x + 1, bar_rect.y + 1,
+    txt(window->title, bar_rect.x + 1, bar_rect.y + 1,
         assets->fonts[ASSET_FONT_SMALL], core);
 
     SDL_FRect btn_region, src_rect;
