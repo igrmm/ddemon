4ec43017abe409fac654e1e223904d8c159bf038
igrmm <igrmmdev@gmail.com>
2024-11-15 22:17:35 -0400

    add ui_widget, ui_style, ui_element and more to ui module

--- src/ui.c
+++ src/ui.c
@@ -9,26 +9,44 @@
 #define UI_MINIM_BUTTON_CODEPOINT 8854
 #define UI_MAXIM_BUTTON_CODEPOINT 8853
 
-void ui_mk_label(struct ui_label *label, struct assets *assets,
+static struct ui_style ui_get_default_style(void)
+{
+    return (struct ui_style){.background_color = {0.16f, 0.16f, 0.21f, 0},
+                             .foreground_color = {0.74f, 0.58f, 0.98f, 0},
+                             .font_color = {0.27f, 0.28f, 0.35f, 0},
+                             .hover_color = {0.23f, 0.24f, 0.31f, 0},
+                             .click_color = {0.21f, 0.22f, 0.30f, 0}};
+}
+
+void ui_mk_label(struct ui_element *label, struct assets *assets,
                  struct core *core)
 {
-    txt_length(label->text, label->rect.x + label->padding,
-               label->rect.y + label->padding,
-               label->rect.w - label->padding * 2,
-               assets->fonts[ASSET_FONT_SMALL], core);
+    const char *text = label->widget.label.text;
+    float text_x = label->rect.x + label->padding;
+    float text_y = label->rect.y + label->padding;
+    float text_w = label->rect.w - label->padding * 2;
+    txt_length(text, text_x, text_y, text_w, assets->fonts[ASSET_FONT_SMALL],
+               core);
 
     // todo handle events
 }
 
-void ui_mk_window(struct ui_window *window, struct assets *assets,
+void ui_mk_window(struct ui_element *window, struct assets *assets,
                   struct core *core)
 {
+    struct ui_style style;
+    if (window->style == NULL) {
+        style = ui_get_default_style();
+    } else {
+        style = *window->style;
+    }
+
     SDL_FRect pixel_tex_region;
     assets_get_texture_region(assets, ASSET_TEXTURE_PIXEL, &pixel_tex_region);
 
     // draw window
     core_add_drawing_fill_rect(core, &pixel_tex_region, &window->rect,
-                               &window->bg_color);
+                               &style.background_color);
 
     // draw bar
     int bar_height = txt_get_font_height(assets->fonts[ASSET_FONT_SMALL]);
@@ -36,30 +54,30 @@ void ui_mk_window(struct ui_window *window, struct assets *assets,
                           window->rect.y + window->rect.h - bar_height,
                           window->rect.w, bar_height};
     core_add_drawing_fill_rect(core, &pixel_tex_region, &bar_rect,
-                               &window->fg_color);
-    window->row_y = bar_rect.y;
+                               &style.foreground_color);
+    window->widget.window.row_y = bar_rect.y;
 
     // draw window border
     core_add_drawing_rect(core, &pixel_tex_region, &window->rect,
-                          &window->fg_color, 1);
+                          &style.foreground_color, 1);
 
     // draw scale button
     SDL_FRect scale_btn_rect = {
         window->rect.x + window->rect.w - bar_height / 2.0f - 2,
         window->rect.y + 2, bar_height / 2.0f, bar_height / 2.0f};
     core_add_drawing_fill_rect(core, &pixel_tex_region, &scale_btn_rect,
-                               &window->fg_color);
+                               &style.foreground_color);
 
     // draw window title on bar
-    struct ui_label bar_title_label = {
+    struct ui_element bar_title_label = {
         .padding = 1,
         .rect = {.x = bar_rect.x, // will be handled by layout func
                  .y = bar_rect.y, // will be handled by layout func
                  // pref value
                  .h = txt_get_font_height(assets->fonts[ASSET_FONT_SMALL]),
                  // pref value
-                 .w = 120.0f},
-        .text = window->title};
+                 .w = 120},
+        .widget = {.label = {.text = window->widget.window.title}}};
     ui_mk_label(&bar_title_label, assets, core);
 
     SDL_FRect btn_region, src_rect;
--- src/ui.h
+++ src/ui.h
@@ -7,22 +7,37 @@
 #include "core.h"
 
 struct ui_label {
-    struct core_color color;
-    int padding;
-    SDL_FRect rect;
     const char *text;
 };
 
 struct ui_window {
-    struct core_color bg_color, fg_color;
-    SDL_FRect rect;
     int row_y;
     const char *title;
 };
 
-void ui_mk_label(struct ui_label *label, struct assets *assets,
+struct ui_style {
+    struct core_color background_color;
+    struct core_color foreground_color;
+    struct core_color hover_color;
+    struct core_color click_color;
+    struct core_color font_color;
+};
+
+union ui_widget {
+    struct ui_window window;
+    struct ui_label label;
+};
+
+struct ui_element {
+    SDL_FRect rect;
+    int padding;
+    struct ui_style *style;
+    union ui_widget widget;
+};
+
+void ui_mk_label(struct ui_element *label, struct assets *assets,
                  struct core *core);
-void ui_mk_window(struct ui_window *window, struct assets *assets,
+void ui_mk_window(struct ui_element *window, struct assets *assets,
                   struct core *core);
 
 #endif
