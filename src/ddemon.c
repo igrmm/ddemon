097e02286113cbf006bbd691795e93f30a45eff9
igrmm <igrmmdev@gmail.com>
2025-05-03 12:56:45 -0400

    use new sdl3 callbacks

--- src/ddemon.c
+++ src/ddemon.c
@@ -1,3 +1,4 @@
+#define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
@@ -7,82 +8,108 @@
 #include "txt.h"
 #include "ui.h"
 
-int main(int argc, char *argv[])
+struct app {
+    struct core core;
+    struct assets assets;
+    struct ui_element win;
+};
+
+static char fps[12];
+void count_fps(void)
 {
-    struct core core = {0};
-    if (!core_setup(&core, "DDEMON", 800, 600, SDL_WINDOW_FULLSCREEN)) {
-        core_shutdown(&core);
-        return -1;
+    static Uint64 timer;
+    static int frames;
+    Uint64 now = SDL_GetTicks();
+    if (now - timer >= 1000) {
+        SDL_snprintf(fps, SDL_arraysize(fps), "%i", frames);
+        timer = now;
+        frames = 0;
     }
+    frames++;
+}
 
-    struct assets assets = {0};
-    if (!assets_load(&core, &assets)) {
-        SDL_Log("Error loading assets.");
-        assets_dispose(&assets);
-        core_shutdown(&core);
-        return -1;
-    }
+SDL_AppResult SDL_AppInit(void **app, int argc, char *argv[])
+{
+    *app = SDL_calloc(1, sizeof(struct app));
+    if (*app == NULL)
+        return SDL_APP_FAILURE;
 
-    ui_set_font(assets.fonts[ASSET_FONT_SMALL]);
-    struct ui_element win = {.rect = {400, 400, 400, 400},
-                             .type = UI_TYPE_WINDOW,
-                             .data.window = {.title = "TestWindow"}};
+    struct core *core = &((struct app *)*app)->core;
+    struct assets *assets = &((struct app *)*app)->assets;
+    struct ui_element *win = &((struct app *)*app)->win;
 
-    char fps[12] = {0};
-    int frames = 0;
-    Uint64 last_frame_time = 0;
-    bool running = true;
-    while (running) {
-        Uint64 now = SDL_GetTicks();
-        if (now - last_frame_time >= 1000) {
-            SDL_snprintf(fps, SDL_arraysize(fps), "%i", frames);
-            last_frame_time = now;
-            frames = 0;
-        }
-        frames++;
+    if (!core_setup(core, "DDEMON", 800, 600, SDL_WINDOW_FULLSCREEN))
+        return SDL_APP_FAILURE;
 
-        SDL_Event evt;
-        while (SDL_PollEvent(&evt)) {
-            if (evt.type == SDL_EVENT_QUIT)
-                running = false;
+    if (!assets_load(core, assets))
+        return SDL_APP_FAILURE;
 
-            if (evt.type == SDL_EVENT_WINDOW_RESIZED) {
-                core_update_viewport(&core, evt.window.data1, evt.window.data2);
+    ui_set_font(assets->fonts[ASSET_FONT_SMALL]);
+    *win = (struct ui_element){.rect = {400, 400, 400, 400},
+                               .type = UI_TYPE_WINDOW,
+                               .data.window = {.title = "TestWindow"}};
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void *app, SDL_Event *event)
+{
+    struct core *core = &((struct app *)app)->core;
+
+    if (event->type == SDL_EVENT_QUIT)
+        return SDL_APP_SUCCESS;
+
+    if (event->type == SDL_EVENT_WINDOW_RESIZED)
+        core_update_viewport(core, event->window.data1, event->window.data2);
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *app)
+{
+    struct core *core = &((struct app *)app)->core;
+    struct assets *assets = &((struct app *)app)->assets;
+    struct ui_element *win = &((struct app *)app)->win;
+
+    count_fps();
+
+    core_clear_screen(0.5f, 0.0f, 0.0f, 1.0f);
+    core_use_shader(core, assets->shaders[ASSET_SHADER_DEFAULT]);
+    struct core_texture atlas_texture = atlas_get_texture(assets->atlas);
+    core_bind_texture(core, atlas_texture);
+    SDL_FRect src_rect = {0, 0, 32, 32};
+    SDL_FRect dst_rect = {0, 0, 32, 32};
+    SDL_FRect tex_region;
+    assets_get_texture_region(assets, ASSET_TEXTURE_TILEMAP, &tex_region);
+
+    // render 13k images on screen
+    for (int l = 0; l < 6; l++) {
+        for (int x = 0; x < 61; x += 1) {
+            for (int y = 0; y < 35; y += 1) {
+                dst_rect.x = x * 32;
+                dst_rect.y = y * 32;
+                src_rect.x = l * 32;
+                core_add_drawing_tex(core, &tex_region, &src_rect, &dst_rect);
             }
         }
-
-        core_clear_screen(0.5f, 0.0f, 0.0f, 1.0f);
-        core_use_shader(&core, assets.shaders[ASSET_SHADER_DEFAULT]);
-        struct core_texture atlas_texture = atlas_get_texture(assets.atlas);
-        core_bind_texture(&core, atlas_texture);
-        SDL_FRect src_rect = {0, 0, 32, 32};
-        SDL_FRect dst_rect = {0, 0, 32, 32};
-        SDL_FRect tex_region;
-        assets_get_texture_region(&assets, ASSET_TEXTURE_TILEMAP, &tex_region);
-
-        // render 13k images on screen
-        for (int l = 0; l < 6; l++) {
-            for (int x = 0; x < 61; x += 1) {
-                for (int y = 0; y < 35; y += 1) {
-                    dst_rect.x = x * 32;
-                    dst_rect.y = y * 32;
-                    src_rect.x = l * 32;
-                    core_add_drawing_tex(&core, &tex_region, &src_rect,
-                                         &dst_rect);
-                }
-            }
-        }
-        char text[64] = "";
-        SDL_snprintf(text, SDL_arraysize(text), "THIS IS NOT A GAME. FPS=%s",
-                     fps);
-        txt(text, 0, 100, assets.fonts[ASSET_FONT_SMALL], &core);
-        ui_mk_window(&win, &assets, &core);
-        core_draw_queue(&core);
-        core_update_window(core.window);
     }
+    char text[64] = "";
+    SDL_snprintf(text, SDL_arraysize(text), "THIS IS NOT A GAME. FPS=%s", fps);
+    txt(text, 0, 100, assets->fonts[ASSET_FONT_SMALL], core);
+    ui_mk_window(win, assets, core);
+    core_draw_queue(core);
+    core_update_window(core->window);
 
-    assets_dispose(&assets);
-    core_shutdown(&core);
+    return SDL_APP_CONTINUE;
+}
 
-    return 0;
+void SDL_AppQuit(void *app, SDL_AppResult result)
+{
+    if (app != NULL) {
+        struct assets *assets = &((struct app *)app)->assets;
+        struct core *core = &((struct app *)app)->core;
+        assets_dispose(assets);
+        core_shutdown(core);
+        SDL_free(app);
+    }
 }
